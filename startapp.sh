8dfe7c0f58856d911675e71527607631beaa52a8
chris <info@cu-tec.de>
2016-10-12 09:40:30 +0000

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -2,4 +2,5 @@ umask 000
 sudo -s mkdir -p /srv/promet
 sudo -s chown -R nobody:users /srv/promet
 sudo -s chmod 777 /srv/promet
+xterm &
 /usr/lib/promet-erp/prometerp --config-path=/srv/promet --database="/srv/promet/database.db"
