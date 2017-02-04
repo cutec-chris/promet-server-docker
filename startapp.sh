0668d7ca6fc326aa8ce62a17343230e52a20e06c
chris <info@cu-tec.de>
2017-02-04 00:56:32 +0000

    Delete startapp.sh

--- startapp.sh
+++
@@ -1,5 +0,0 @@
-umask 000
-sudo -s mkdir -p /srv/promet
-sudo -s chown -R nobody:users /srv/promet
-sudo -s chmod 777 /srv/promet
-/usr/lib/promet-erp/prometerp --config-path=/srv/promet --database="/srv/promet/database.db"
